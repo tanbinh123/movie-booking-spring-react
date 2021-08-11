ef82818a6ecdfe3a8ba180eb4d0893bfb71db2e6
EMEA\WPires <william.pires@cbre.com>
2021-08-11 11:36:08 +0200

    Create React App + Bootstrap Custom Color Palette

---
+++ movie-booking-admin/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <div>
+        <div class="container bg-dark text-primary">
+          <h1>Hello World!</h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
+
